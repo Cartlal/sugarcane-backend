4a88d241d2f2a826685e4b714d79772f630082a9
Cartlal <147944223+Cartlal@users.noreply.github.com>
2025-11-13 17:04:57 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String,  },
-  email: { type: String, unique: true },
-  password: { type: String,  },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 module.exports = mongoose.model("User", userSchema);
